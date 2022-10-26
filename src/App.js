762f1ac9c46333f57d5342194d18c3b27680b6ff
Luong Dang <dangvluong@gmail.com>
2022-10-26 21:55:39 +0700

    04-useState basic example TodoList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'I love Easy Frontend!' },
+    { id: 2, title: 'We love Easy Frontend!' },
+    { id: 3, title: 'They love Easy Frontend!' }
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex(a => a.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoClick = [...todoList];
+    newTodoClick.splice(index, 1);
+    setTodoList(newTodoClick);
+  }
+
   return (
     <div className="App">
       <h1>Welcome to React Hooks!</h1>
 
-      <ColorBox></ColorBox>
+      <TodoList todos={todoList}
+        onTodoClick={handleTodoClick}>
+
+      </TodoList>
     </div>
   );
 }
