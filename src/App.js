18890afa454ebfd2bf7e1c778bb05ca782329f7a
Luong Dang <dangvluong@gmail.com>
2022-10-26 22:15:58 +0700

    05-useState basic example TodoForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
@@ -18,15 +19,30 @@ function App() {
     newTodoClick.splice(index, 1);
     setTodoList(newTodoClick);
   }
+  function handleTodoFormSubmit(formValues) {
+    console.log('Form submit: ', formValues);
+
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues
+    };
+
+    const newTodoList = [...todoList,];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
 
   return (
     <div className="App">
       <h1>Welcome to React Hooks!</h1>
 
+      <TodoForm onSubmit={handleTodoFormSubmit}></TodoForm>
+
       <TodoList todos={todoList}
         onTodoClick={handleTodoClick}>
-
       </TodoList>
+
+
     </div>
   );
 }
